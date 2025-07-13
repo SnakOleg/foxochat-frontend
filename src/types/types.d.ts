804bf5b11bcec017ed96238cbc11a0653d7a48cf
snakoleg <ochsherbina@gmail.com>
2025-07-13 04:50:47 +0300

    Add SVG module type declaration for Preact

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,1 +1,7 @@
 declare module "@fontsource/inter";
+
+declare module "*.svg?react" {
+  import { ComponentType } from "preact";
+  const content: ComponentType<{ className?: string; style?: any }>;
+  export default content;
+}
